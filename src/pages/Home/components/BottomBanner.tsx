40208a7008058177837ad07fb7b09a6b466efe27
howydev <qwertity@gmail.com>
2021-03-31 01:56:21 -0400

    added responsiveness for bottom banner

--- src/pages/Home/components/BottomBanner.tsx
+++ src/pages/Home/components/BottomBanner.tsx
@@ -1,4 +1,4 @@
-import { Box, makeStyles, Typography, useMediaQuery } from '@material-ui/core';
+import { Box, makeStyles, Typography, Theme, useTheme, useMediaQuery } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) => ({
   contentContainer: {
@@ -12,6 +12,8 @@ const useStyles = makeStyles((theme: Theme) => ({
 
 const BottomBanner: React.FC = () => {
   const classes = useStyles();
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('md'));
 
   let menuList = [
     {
@@ -48,24 +50,27 @@ const BottomBanner: React.FC = () => {
   ];
 
   return (
-    <Box className={classes.contentContainer} paddingY={10}>
-      <Box display="flex" justifyContent="center">
-        {menuList.map((value) =>
-          <Box key={value.title} display="flex" flexDirection="column" paddingX={10}>
-            <Box marginBottom={1.5}>
-              <Typography variant='subtitle2'>{value.title}</Typography>
-            </Box>
-            {value.items.map((elem) =>
-              <Box key={elem.title} marginBottom={1.5}>
-                <Typography variant='body1'>{elem.title}</Typography>
+    <Box className={classes.contentContainer}>
+      {matches ?
+        <>
+          <Box display="flex" justifyContent="center" paddingY={6}>
+            {menuList.map((value) =>
+              <Box key={value.title} display="flex" flexDirection="column" paddingX={10}>
+                <Box marginBottom={1.5}>
+                  <Typography variant='subtitle2'>{value.title}</Typography>
+                </Box>
+                {value.items.map((elem) =>
+                  <Box key={elem.title} marginBottom={1.5}>
+                    <Typography variant='body1'>{elem.title}</Typography>
+                  </Box>
+                )}
               </Box>
             )}
           </Box>
-        )}
-      </Box>
-      <Box display="flex" justifyContent="center">
-        <Typography fontSize="0.81rem"> @ 2021 Impossible. Finance </Typography>
-      </Box>
+          <Box display="flex" justifyContent="center" paddingY={10}>
+            <Typography variant='body2'> @ 2021 Impossible. Finance </Typography>
+          </Box>
+        </> : <></>}
     </Box>
   );
 };
