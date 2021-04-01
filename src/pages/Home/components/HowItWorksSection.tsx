68a4659be49dccb6e0e1493d77b2839404b6231e
acceleratedmonkey <79084785+acceleratedmonkey@users.noreply.github.com>
2021-04-01 23:15:30 +0800

    update how it works section for mobile (#6)
    Co-authored-by: Accelerated Monkey <acceleratedmonkey@protonmail.com>

--- src/pages/Home/components/HowItWorksSection.tsx
+++ src/pages/Home/components/HowItWorksSection.tsx
@@ -5,6 +5,8 @@ import {
   makeStyles,
   Theme,
   Typography,
+  useMediaQuery,
+  useTheme,
 } from '@material-ui/core';
 import React from 'react';
 import InfoCard from '../../../common/components/InfoCard';
@@ -16,6 +18,9 @@ const useStyles = makeStyles((theme: Theme) => ({
     justifyContent: 'center',
     justifyItems: 'center',
   },
+  container: {
+    padding: 0,
+  },
   title: {
     fontWeight: 800,
   },
@@ -23,13 +28,18 @@ const useStyles = makeStyles((theme: Theme) => ({
     fontWeight: 700,
     color: '#0074FF',
   },
+  infoCardContainer: {
+    float: 'left',
+  },
 }));
 
 const HowItWorksSection: React.FC = () => {
   const classes = useStyles();
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('xs'));
   return (
     <Box className={classes.contentContainer} paddingY={8}>
-      <Container maxWidth='lg'>
+      <Container maxWidth='lg' className={classes.container}>
         <Grid container>
           <Grid item xs={12}>
             <Typography variant='h1' align='center' className={classes.title}>
@@ -39,24 +49,33 @@ const HowItWorksSection: React.FC = () => {
               Here’s how it works
             </Typography>
             <Box marginTop={5} />
-            <Box display='flex' flexWrap='wrap'>
-              <InfoCard
-                title='Swap'
-                description='Bye bye complicated interfaces. You have token A and want B? 
-Select, click, confirm, that’s it.'
-                icon='/images/trade.png'
-              />
-              <InfoCard
-                title='Add Liquidity'
-                description='Earn fees by adding say tokens A and B. Anyone who swaps between them pays you a fee.*'
-                icon='/images/liquidity.png'
-              />
-              <InfoCard
-                title='Stake to get Rewards'
-                description='Show proof of liquidity (staking) to supercharge earnings in the form of IF token rewards.'
-                icon='/images/earn2.png'
-              />
+            <Box overflow='auto'>
+              <Box
+                display='flex'
+                flexWrap={matches ? 'nowrap' : 'wrap'}
+                className={classes.infoCardContainer}
+                paddingLeft={3}
+              >
+                <InfoCard
+                  title='Swap'
+                  description='Bye bye complicated interfaces. You have token A and want B? 
+                  Select, click, confirm, that’s it.'
+                  icon='/images/trade.png'
+                />
+                <InfoCard
+                  title='Add Liquidity'
+                  description='Earn fees by adding say tokens A and B. Anyone who swaps between them pays you a fee.*'
+                  icon='/images/liquidity.png'
+                />
+                <InfoCard
+                  title='Stake to get Rewards'
+                  description='Show proof of liquidity (staking) to supercharge earnings in the form of IF token rewards.'
+                  icon='/images/earn2.png'
+                />
+                <Box marginRight={4} />
+              </Box>
             </Box>
+
             <Box marginTop={5} />
             <Box justifyContent='center' alignItems='center'>
               <Typography
