d1b16bf3faad0eb1943c8528f64f266606cb43c3
Accelerated Monkey <acceleratedmonkey@protonmail.com>
2021-04-24 14:03:48 +0800

    Fix wrong url

--- src/common/components/LinksJson/index.tsx
+++ src/common/components/LinksJson/index.tsx
@@ -2,7 +2,7 @@ const LinksJson = [
   {
     title: 'Products',
     items: [
-      { title: 'Swap', url: `${process.env.NEXT_PUBLIC_DOMAIN}/#/swap` },
+      { title: 'Swap', url: `${process.env.NEXT_PUBLIC_SWAP_DOMAIN}/#/swap` },
       {
         title: 'Liquidity',
         url: `${process.env.NEXT_PUBLIC_SWAP_DOMAIN}/#/pool`,
