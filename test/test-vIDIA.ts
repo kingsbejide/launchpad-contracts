13554d12ed53f530cfa78635108e584b1c3087c8
Michael <m3k0c6@gmail.com>
2022-03-22 09:04:52 -0400

    fixed indentation

--- test/test-vIDIA.ts
+++ test/test-vIDIA.ts
@@ -26,7 +26,7 @@ export default describe('vIDIA', function () {
   beforeEach(async function () {
     // Get the ContractFactory and Signers here.
     // Token = await ethers.getContractFactory("Token");
-    [owner, vester] = await ethers.getSigners()
+    ;[owner, vester] = await ethers.getSigners()
 
     // To deploy our contract, we just have to call Token.deploy() and await
     // for it to be deployed(), which happens once its transaction has been
@@ -49,7 +49,6 @@ export default describe('vIDIA', function () {
     )
   })
 
-
   it('deploys', async function () {
     // get owner
     const [owner] = await ethers.getSigners()
@@ -67,11 +66,9 @@ export default describe('vIDIA', function () {
   })
 
   it('sets penalty of a token', async function () {
-
     const penalty = 10
 
     await vIDIA.updateSkipUnstakeDelayFee(penalty)
-
 
     expect((await vIDIA.skipUnstakeDelayFee()).toNumber()).to.eq(penalty)
   })
@@ -79,14 +76,12 @@ export default describe('vIDIA', function () {
   it('cannot set penalty of a token', async function () {
     const penalty = 10
 
-    await expect
-    (vIDIA.connect(vester).updateSkipUnstakeDelayFee(penalty)
+    await expect(
+      vIDIA.connect(vester).updateSkipUnstakeDelayFee(penalty)
     ).to.be.revertedWith('Must have fee setter role')
-
   })
 
   it('sets delay of a token', async function () {
- 
     const delay = 10
 
     await vIDIA.updateUnvestingDelay(delay)
@@ -95,18 +90,20 @@ export default describe('vIDIA', function () {
   })
 
   it('deploys and cannot set delay of a token, thus still 0', async function () {
-
     const delay = 10
 
-    await expect
-    (vIDIA.connect(vester).updateUnvestingDelay(delay)
+    await expect(
+      vIDIA.connect(vester).updateUnvestingDelay(delay)
     ).to.be.revertedWith('Must have delay setter role')
   })
 
   it('deploys and stakes tokens', async function () {
     const transferAmt = 10000000
-    await VestToken.transfer(vester.address, transferAmt) 
-    await VestToken.connect(vester).approve(vIDIA.address, ethers.constants.MaxUint256)
+    await VestToken.transfer(vester.address, transferAmt)
+    await VestToken.connect(vester).approve(
+      vIDIA.address,
+      ethers.constants.MaxUint256
+    )
     const firstStakeAmt = 100
     const secondStakeAmt = 250
     await vIDIA.connect(vester).stake(firstStakeAmt)
@@ -120,8 +117,11 @@ export default describe('vIDIA', function () {
 
   it('stakes and unstakes tokens', async function () {
     const transferAmt = 10000000
-    await VestToken.transfer(vester.address, transferAmt) 
-    await VestToken.connect(vester).approve(vIDIA.address, ethers.constants.MaxUint256)
+    await VestToken.transfer(vester.address, transferAmt)
+    await VestToken.connect(vester).approve(
+      vIDIA.address,
+      ethers.constants.MaxUint256
+    )
     const firstStakeAmt = 100
     const secondStakeAmt = 250
     await vIDIA.connect(vester).stake(firstStakeAmt)
@@ -132,15 +132,14 @@ export default describe('vIDIA', function () {
     totalStaked = (await vIDIA.totalStakedAmount()).toNumber()
     expect(totalStaked).to.eq(firstStakeAmt + secondStakeAmt)
     await vIDIA.connect(vester).unstake(secondStakeAmt)
-    const userData = (await vIDIA.userInfo(vester.address))
+    const userData = await vIDIA.userInfo(vester.address)
     expect(userData.unstakedAmount).to.eq(secondStakeAmt)
-    const unstakeTime = (await getBlockTime()) + (await vIDIA.unstakingDelay()).toNumber()
+    const unstakeTime =
+      (await getBlockTime()) + (await vIDIA.unstakingDelay()).toNumber()
     expect(userData.unstakeAt).to.eq(unstakeTime)
-    await expect
-    (vIDIA.connect(vester).unstake(firstStakeAmt)
+    await expect(
+      vIDIA.connect(vester).unstake(firstStakeAmt)
     ).to.be.revertedWith('User has pending tokens unstaking')
-
-
   })
 
   it('test whitelist feature', async () => {
@@ -238,12 +237,12 @@ export default describe('vIDIA', function () {
     let contractPrevUnderlying = await underlying.balanceOf(vidia.address)
     let prevFee = await vidia.accumulatedFee()
 
-
     const feePercentBasisPts = await vidia.skipUnstakeDelayFee()
 
     for (let i = 0; i < testCases.length; i++) {
-      
-      const fee = feePercentBasisPts.mul(testCases[i]).div(ethers.BigNumber.from('10000')) // 10000 basis pts = 100%
+      const fee = feePercentBasisPts
+        .mul(testCases[i])
+        .div(ethers.BigNumber.from('10000')) // 10000 basis pts = 100%
       const receiveAmt = testCases[i].sub(fee)
 
       const reward = await vidia.calculateUserReward()
@@ -254,14 +253,22 @@ export default describe('vIDIA', function () {
         .to.emit(underlying, 'Transfer')
         .withArgs(vidia.address, owner.address, receiveAmt)
 
-      expect(await vidia.balanceOf(owner.address)).to.equal(userPrevVidiaBalance.sub(testCases[i]))
-      expect(await underlying.balanceOf(owner.address)).to.equal(userPrevUnderlying.add(receiveAmt).add(reward))
+      expect(await vidia.balanceOf(owner.address)).to.equal(
+        userPrevVidiaBalance.sub(testCases[i])
+      )
+      expect(await underlying.balanceOf(owner.address)).to.equal(
+        userPrevUnderlying.add(receiveAmt).add(reward)
+      )
       expect(await vidia.accumulatedFee()).to.equal(prevFee.add(fee))
-      expect(await underlying.balanceOf(vidia.address)).to.equal(contractPrevUnderlying.sub(receiveAmt).sub(reward))
+      expect(await underlying.balanceOf(vidia.address)).to.equal(
+        contractPrevUnderlying.sub(receiveAmt).sub(reward)
+      )
 
       userPrevVidiaBalance = userPrevVidiaBalance.sub(testCases[i])
       userPrevUnderlying = userPrevUnderlying.add(receiveAmt).add(reward)
-      contractPrevUnderlying = contractPrevUnderlying.sub(receiveAmt).sub(reward)
+      contractPrevUnderlying = contractPrevUnderlying
+        .sub(receiveAmt)
+        .sub(reward)
       prevFee = prevFee.add(fee)
     }
   })
